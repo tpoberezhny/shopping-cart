9b30363020160af888314085aa6a40de5be1211e
Tim <poberezhny003@gmail.com>
2023-07-03 23:35:13 +0200

    Context, get/increase/decrease/remove functions

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Store } from "./pages/Store";
 import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
   return (
@@ -13,12 +14,14 @@ function App() {
         <div className="gradient" />
       </div>
       <div className="max-w-[1240px] md:pl-10 px-3 w-full mx-auto">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <ShoppingCartProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </ShoppingCartProvider>
       </div>
     </main>
   );
