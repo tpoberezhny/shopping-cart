a154bee6f44567bf909eb7a5a5a330c12492b8de
Tim <poberezhny003@gmail.com>
2023-07-04 00:58:24 +0200

    Adding event listeners

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,4 @@
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -8,7 +9,14 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 0;
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
   return (
     <div className="bg-white shadow-2xl min-h-[500px] group rounded-xl">
       {/* image section */}
@@ -29,17 +37,17 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
       {/* add/count section */}
       <div className="flex justify-center item-center mx-auto w-[250px]">
         {quantity === 0 ? (
-          <button className="btn">Add to cart</button>
+          <button className="btn" onClick={() => increaseCartQuantity(id)}>Add to cart</button>
         ) : (
           <div className="flex items-center flex-col gap-2">
             <div className="flex items-center justify-center gap-2 mt-[-15px] mb-2">
-              <button className="btn w-[50px]">-</button>
+              <button className="btn w-[50px]" onClick={() => decreaseCartQuantity(id)}>-</button>
               <div>
                 <span>{quantity} in cart</span>
               </div>
-              <button className="btn">+</button>
+              <button className="btn" onClick={() => increaseCartQuantity(id)}>+</button>
             </div>
-            <button className="dangerBtn">Remove</button>
+            <button className="dangerBtn" onClick={() => removeFromCart(id)}>Remove</button>
           </div>
         )}
       </div>
